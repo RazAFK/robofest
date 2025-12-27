aef6dd361d6e7b737779285de19a8149ef1d3a2c
RazeManer <mr.yger001@gmail.com>
2025-12-27 19:46:30 +0300

    main_change

--- main_test.py
+++ main_test.py
@@ -1,2 +1,14 @@
-f = False
-print(f is None)
+tw = [2**x for x in range(1, 7)]
+tr = [3**x for x in range(1, 5)]
+t2 = 0
+for i in tw:
+    for x in range(i, 101, i):
+        t2+=1
+print(t2)
+t3 = 0
+for i in tr:
+    for x in range(i, 101, i):
+        t3+=1
+print(t3)
+
+print(2**(100-t2)*3**(100-t3))
