cf0b66d351a9bde22adab08203398a7f7af32911
RazeManer <mr.yger001@gmail.com>
2026-01-08 12:34:04 +0300

    missed file

--- find_arduinos.py
+++ find_arduinos.py
@@ -1,6 +1,15 @@
 from connector.arduino_class import take_arduinos
+from datetime import datetime
+
+start = datetime.now()
 
 wheels, manipulator = take_arduinos()
 
 print(wheels)
 print(manipulator)
+
+end = datetime.now()
+
+print('total time:', end-start)
+
+
