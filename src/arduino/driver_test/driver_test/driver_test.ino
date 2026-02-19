1aa4ecd7f05932d41be21b1c900a836ca065c677
RazeManer <mr.yger001@gmail.com>
2026-02-19 09:48:14 +0300

    arduino update

---
+++ src/arduino/driver_test/driver_test/driver_test.ino
@@ -0,0 +1,41 @@
+#include <Wire.h>
+#include <iarduino_I2C_Motor.h>
+
+iarduino_I2C_Motor mot(0x09); 
+
+// // Параметры вашего мотора
+// const float REDUCER = 99.0;
+// const float MAGNETS = 7.0;
+// const float WHEEL_RADIUS_CM = 3.0; // Ваш радиус в см
+
+// void setup() {
+//     mot.begin();
+//     mot.setMagnet(MAGNETS);
+//     mot.setReducer(REDUCER);
+//     mot.radius = WHEEL_RADIUS_CM;
+// }
+
+// void loop() {
+//     // 1. Проехать 5 оборотов
+//     mot.setSpeed(100, MOT_RPM, 1, MOT_REV);
+//     delay(2000);
+    
+//     // mot.setSpeed(70, MOT_RPM, 5, MOT_SEC);
+//     // delay(5000);
+// }
+
+void setup() {
+    Serial.begin(9600);
+    mot.begin();
+    // Сбрасываем настройки в 1, чтобы видеть "чистые" данные
+    mot.setMagnet(7);
+    mot.setReducer(94.5);
+}
+
+void loop() {
+    // Выводим текущее положение в тиках (или оборотах, которые сейчас равны тикам)
+    Serial.print("Position: ");
+    Serial.println(mot.getSum(MOT_REV));
+    mot.setSpeed(100, MOT_RPM, 1, MOT_REV);
+    delay(2000);
+}
