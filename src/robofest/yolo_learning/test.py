e70507e14a23f53a7a7cc1a569a096f6c1a7cff7
RazeManer <mr.yger001@gmail.com>
2026-02-22 02:58:00 +0300

    Update test.py

--- src/robofest/yolo_learning/test.py
+++ src/robofest/yolo_learning/test.py
@@ -1,4 +1,6 @@
-import os
+import cv2
 
-path = 'C:/Users/admin/Desktop/cube_photos/'
-print(os.path.abspath(path))
+frame = cv2.imread(r'D:\AudioBooks\HarryPotter\Rowling.J._Harry_Potter_i_taynaya_komnata.[torrents.ru]\cover.jpg')
+
+while True:
+    cv2.imshow('frame', frame)
