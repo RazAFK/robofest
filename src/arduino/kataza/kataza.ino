7514838d35b67bc08c20ea6e0d9faad8f5db901e
RazeManer <mr.yger001@gmail.com>
2026-03-02 10:33:30 +0300

    sosi gosha

--- src/arduino/kataza/kataza.ino
+++ src/arduino/kataza/kataza.ino
@@ -3,13 +3,22 @@ SoftTwoWire sWire(A4, A5);
 
 #include <iarduino_I2C_Motor.h>
 
-iarduino_I2C_Motor mot1(0x09);
-iarduino_I2C_Motor mot2(0x0A);
-iarduino_I2C_Motor mot3(0x0B);
-iarduino_I2C_Motor mot4(0x0C);
+// iarduino_I2C_Motor mot_test(0x09);
+iarduino_I2C_Motor mot1(0x0A);
+iarduino_I2C_Motor mot2(0x0B);
+iarduino_I2C_Motor mot3(0x0C);
+iarduino_I2C_Motor mot4(0x0D);
 
 void setup() {
+    Serial.begin(9600);
     sWire.begin();
+
+    // mot_test.begin(&sWire);
+
+    // mot_test.setReducer(7);
+    // mot_test.setMagnet(56);
+
+    // mot_test.setSpeed(-60, MOT_RPM, 3, MOT_REV);
 
     mot1.begin(&sWire);
     mot2.begin(&sWire);
@@ -18,19 +27,27 @@ void setup() {
     
     mot1.setReducer(7);
     mot1.setMagnet(56);
+
     mot2.setReducer(7);
     mot2.setMagnet(56);
+    
     mot3.setReducer(7);
     mot3.setMagnet(56);
+    
     mot4.setReducer(7);
     mot4.setMagnet(56);
 
-    mot1.setSpeed(60, MOT_RPM, 3, MOT_REV);
+    Serial.println("bebebe");
+
+    mot1.setSpeed(-60, MOT_RPM, 3, MOT_REV);
     mot2.setSpeed(60, MOT_RPM, 3, MOT_REV);
     mot3.setSpeed(60, MOT_RPM, 3, MOT_REV);
-    mot4.setSpeed(60, MOT_RPM, 3, MOT_REV);
+    mot4.setSpeed(-60, MOT_RPM, 3, MOT_REV);
+
+    
+    Serial.println("bububu");
+
 }
 
 void loop() {
-
 }
